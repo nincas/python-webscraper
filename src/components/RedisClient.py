749cfb7d4d5277b7f3c013b1f0597e43d56ceded
nino.home <nino.invech3e@gmail.com>
2020-10-06 21:44:20 +0800

    updates

--- src/components/RedisClient.py
+++ src/components/RedisClient.py
@@ -5,9 +5,11 @@ class RedisClient:
     client = None
     pubsub = None
     isSub = False
+    host = '127.0.0.1'
+    password = '' # Ccnkbq9V4KDVCyT5FfYpH7ZPhcvisYCf
 
     def __init__(self): 
-        self.client = redis.Redis(host='10.19.3.24', port=6379, password='Ccnkbq9V4KDVCyT5FfYpH7ZPhcvisYCf')
+        self.client = redis.Redis(host=self.host, port=6379, password=self.password)
 
     def getClient(self):
         return self.client
