b5727986914f27ea0916fbd8c10c46a3712af498
nino.invech <nino.invech3e>
2021-02-26 03:41:33 +0800

    Change to geckdriver

--- src/Bybit.py
+++ src/Bybit.py
@@ -1,5 +1,6 @@
 from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
+# from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.firefox.options import Options
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
@@ -8,7 +9,7 @@ from selenium.common import exceptions
 from selenium.common.exceptions import StaleElementReferenceException
 from selenium.webdriver.support import expected_conditions as EC
 from time import sleep
-from .components.RedisClient import RedisClient
+# from .components.RedisClient import RedisClient
 from .components.Currency import Currency
 import os
 import sys
@@ -22,20 +23,19 @@ class Bybit:
         self.src = srcUrl
 
     def scrape(self, file):
-        print(self.src)
         PATH = os.path.abspath(file)
 
         # Set browser
         chrome_options = Options()
         chrome_options.add_argument('--headless')
-        chrome_options.add_argument('--no-sandbox')
-        chrome_options.add_argument("--disable-gpu")
-        
-        browser = webdriver.Chrome(ChromeDriverManager().install(), chrome_options=chrome_options)
+        chrome_options.add_argument('--disable-gpu')
+
+        browser = webdriver.Firefox(options=chrome_options)
 
         # Load the page
         browser.get(self.src)
         browser.implicitly_wait(10)
+        browser.set_page_load_timeout(30)
         r = RedisClient()
         cur = Currency()
         redisKey = 'btc-value-bybit'
@@ -44,9 +44,10 @@ class Bybit:
             try:
                 # Wait until the element appear on the src page.
                 priceItem = WebDriverWait(browser, 10).until(
-                    EC.presence_of_element_located((By.XPATH, "//span[contains(@class, 'price')]"))
+                    EC.element_to_be_clickable((By.XPATH, "//span[contains(@class, 'price')]"))
                 )
                 
+                # print(priceItem.text)
                 indicator = "-" if float(self.lastPrice) > float(priceItem.text.replace(",", "")) else "+"
                 
                 if (self.lastPrice == priceItem.text.replace(",", "")):
